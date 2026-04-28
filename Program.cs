e376b2b098adb735c51e35c983a05b58505ef8fa
lwmgu <gustavomcassimiro11@gmail.com>
2026-04-28 15:11:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+namespace pasta_curso3;
+
+class Program
+{
+    static void Main(string[] args){
+
+//     {Console.WriteLine("digite um numero: ");
+//         Console.WriteLine("Hello, World!");
+//         int numero = 9;
+//         if (numero > 5){
+//         Console.WriteLine("hello amanda");
+// Console.WriteLine
+        // }
+// int idade = 15;
+
+//     if(idade < 16)
+//     {
+//         Console.WriteLine("não pode votar");
+//     }
+//     else if (idade < 18)
+//     {
+//         Console.WriteLine("voto opcional");
+//     } 
+//     else
+//     {
+//         Console.WriteLine("voto obrigatório");
+
+//     }
+
+// int nota = 10;
+
+// if(nota > 9)
+// {
+//     Console.WriteLine("aprovado e ganhou chocolate");
+// }
+// else if (nota < 5)
+// {}
+
+// Console.Write("usuario: ");
+// string usuario = Console.ReadLine();
+
+// Console.Write("senha: ");
+// string senha = Console.ReadLine();
+
+// if(usuario == "admin" && senha == "1234")
+// {
+//     Console.WriteLine("acesso permitido");
+// }
+// else
+// {Console.WriteLine("acesso negado");
+// }
+
+//     }
+    
+//     }    
+// Console.Write("digite um numero: ");
+// int numero = int.Parse(Console.ReadLine());
+
+// string resultado = numero % 2 == 0 ? "par" : "impar";
+
+// Console.WriteLine(resultado);
+
+// int idade = 12;
+
+// Console.WriteLine(idade >=18 ? "maior de idade" : "menor de idade");
+
+// Console.Write("digite um numero de 1 a 3: ");
+// int opcao = int.Parse(Console.ReadLine());
+
+// switch(opcao)
+// {
+//     case 1:
+//     Console.WriteLine("voce escolheu a opcao 1");
+//     break;
+//     case 2:
+//     Console.WriteLine("voce escolheu a opcao 2");
+//     break;
+//     case 3:
+//     Console.WriteLine("voce escolheu a opcao 3");
+//     break;
+//     default:
+//     Console.WriteLine("opcao invalida");
+// break;
+// }
+
+
+
+
+}
+
+
+    }}
+    
